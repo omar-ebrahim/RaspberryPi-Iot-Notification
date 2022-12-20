9d06516d21c9fd85506d926dba49e6e933cda852
Omar Ebrahim <o_ebrahim1@outlook.com>
2022-12-20 12:23:13 +0000

    Notification should be shown on screen

--- device.py
+++ device.py
@@ -1,31 +1,67 @@
 import time
 from azure.iot.device import IoTHubDeviceClient
+import json
+import platform
 
-RECEIVED_MESSAGES = 0
+# These will not work on Windows, so testing must be done on the RPi.
+import RPi.GPIO as GPIO
+import RGB1602
 
 CONNECTION_STRING = "HostName=oe-iot.azure-devices.net;DeviceId=omar-rpi;SharedAccessKey=IH0lOtSxLo/q3hARsr7xPzp5zmeoYG5yBddYFNclxmM="
+NOTIFICATION_KEY = "notification"
+
+lcd = RGB1602.RGB1602(16, 2)
+
 
 def message_handler(message):
-        global RECEIVED_MESSAGES
-        RECEIVED_MESSAGES +=1
-        print("message received")
-        for property in vars(message).items():
-                print("    {}".format(property))
+    """
+    Handles the message from Azure IoT Hub
+    """
+    lcd.setCursor(0, 0)
+    lcd.printout("new message")
+
+    print("message received")
+    message_dict = vars(message)
+    # It has a b' indicating it's a byte array so can't be parsed as valid JSON
+    # so convert it to a string, remove the b' and last single-quote and load it as JSON again
+    message_data = str(message_dict["data"])
+    message_data = message_data[2:len(message_data)-1]
+    notification_data = json.loads(message_data)
+    if NOTIFICATION_KEY in notification_data:
+        notification_message = notification_data[NOTIFICATION_KEY]
+        time.sleep(2)
+        lcd.clear()
+        lcd.setCursor(0, 0)
+        lcd.printout(str(notification_message))
+        message_length = len(notification_message)
+        if message_length > 16:
+            # only scroll if we need to
+            scroll_amount = len(notification_message) - 16 # e.g. 2
+            for x in range(scroll_amount): # 0, 1
+                time.sleep(1)
+                lcd.scrollLeft()
+
 
 def main():
-        print("starting ...")
-        client =  IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-        print("Waiting for messages. Press Ctrl-C to exit")
-        try:
-                client.on_message_received = message_handler
-                client.connect()
-                while True:
-                        time.sleep(10)
-        except KeyboardInterrupt:
-                print("user stopped service")
-        finally:
-                print("Shutting down")
-                client.shutdown()
+    print("starting ...")
+    print(platform.platform())
+    client = IoTHubDeviceClient.create_from_connection_string(
+        CONNECTION_STRING)
+    print("Waiting for messages. Press Ctrl-C to exit")
+
+    lcd.clear()
+
+    try:
+        client.on_message_received = message_handler
+        client.connect()
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print("user stopped service")
+    finally:
+        print("Shutting down")
+        client.shutdown()
+
 
 if __name__ == '__main__':
-        main()
+    main()
