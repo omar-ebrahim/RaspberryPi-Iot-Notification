cf7607b0be1e6450d978f1bb84aa86f17a82d436
Omar Ebrahim <o_ebrahim1@outlook.com>
2022-12-20 10:49:44 +0000

    Removed comments

--- oe_demo.py
+++ oe_demo.py
@@ -9,18 +9,7 @@ colour_red = (255, 0, 0)
 colour_green = (0, 255, 0)
 colour_blue = (0, 0, 255)
 
-#lcd.setCursor(0, 0)
-#lcd.printout("Hola! my name is")
-#lcd.setCursor(0, 1)
-#lcd.printout("omar")
-
-#time.sleep(5)
-
 lcd.clear()
-#lcd.setCursor(0, 0)
-#lcd.printout("I'm turning off")
-#lcd.setCursor(0, 1)
-#lcd.printout("Goodbye!");
 
 time.sleep(1)
 lcd.show_cursor()
@@ -37,26 +26,3 @@ lcd.scrollLeft()
 time.sleep(1)
 lcd.scrollLeft()
 
-
-#lcd.show_cursor()
-
-#lcd.command(0x04)
-#lcd.command(0x04)
-#lcd.command(0x04)
-#lcd.command(0x04)
-#lcd.command(0x04)
-#lcd.command(0x04)
-
-
-#lcd.command(0x18)
-#time.sleep(1)
-#lcd.command(0x18)
-#time.sleep(1)
-#lcd.command(0x18)
-#lcd.setCursor(5, 0)
-#lcd.scrollLeft() # Move right
-#time.sleep(1)
-#lcd.scrollRight()
-#time.sleep(1)
-#lcd.command(0x04)
-#lcd.clear()
